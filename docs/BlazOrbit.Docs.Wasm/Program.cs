d6f2ca80d41d9d60c6b0ad68328b066740f171e0
smaicas <samuel.maicas.development@gmail.com>
2026-05-03 23:41:17 +0200

    docs: improve docs concepts

--- docs/BlazOrbit.Docs.Wasm/Program.cs
+++ docs/BlazOrbit.Docs.Wasm/Program.cs
@@ -11,6 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddBlazOrbit();
+
 builder.Services.AddBlazOrbitLocalizationWasm(options =>
 {
     options.SupportedCultures =
